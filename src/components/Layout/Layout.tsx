35815b8371ad8172027824a364a58a0c40204f33
jjanmo <jjanmo07@hanmail.net>
2023-09-10 17:28:18 +0900

    chores: fix minor styles

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,3 +1,4 @@
+import BottomBar from '@components/BottomBar'
 import { colors } from '@styles/theme'
 import { PropsWithChildren } from 'react'
 import styled from 'styled-components'
@@ -5,7 +6,11 @@ import styled from 'styled-components'
 export default function Layout({ children }: PropsWithChildren) {
   return (
     <Container>
-      <Main>{children}</Main>
+      <Main>
+        {children}
+
+        <BottomBar />
+      </Main>
     </Container>
   )
 }
@@ -14,10 +19,12 @@ const Container = styled.div`
   width: 100%;
   height: 100vh;
   background-image: linear-gradient(179.59deg, rgb(11, 185, 247) 11.88%, rgb(255, 255, 255) 100.6%);
+  overflow: hidden;
 `
 const Main = styled.main`
+  position: relative;
   width: 42rem;
-  min-height: 100vh;
+  height: 100vh;
   margin: 0 auto;
   background-color: ${colors.black};
 `
