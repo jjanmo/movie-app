845df06ce4aa4a8090b89581c3f6f48b674c1e76
jjanmo <jjanmo07@hanmail.net>
2023-09-11 23:58:50 +0900

    feat: provider 추가(커밋늦게진행)

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './router.tsx'
 import GlobalStyles from '@styles/GlobalStyles.ts'
+import { RecoilRoot } from 'recoil'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <GlobalStyles />
-    <RouterProvider router={router} />
+    <RecoilRoot>
+      <GlobalStyles />
+      <RouterProvider router={router} />
+    </RecoilRoot>
   </React.StrictMode>
 )
