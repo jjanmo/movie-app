6af57bdbeee93317e3391f1c086422bccfda3523
jjanmo <jjanmo07@hanmail.net>
2023-09-11 14:21:58 +0900

    fix: header 설정 수정 - cause call error

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -3,9 +3,6 @@ import axios from 'axios'
 export const API = axios.create({
   baseURL: 'http://www.omdbapi.com',
   headers: {
-    'X-Requested-With': 'XMLHttpRequest',
     'Content-Type': 'application/json',
   },
 })
-
-// https://www.omdbapi.com/?apikey=92e32667&s=iron%20man&page=2
