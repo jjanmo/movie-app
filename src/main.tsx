18e2f8884b549262a82699d903f75793799c04f3
jjanmo <jjanmo07@hanmail.net>
2023-09-13 22:36:53 +0900

    fix: react-beautiful-dnd에서 id 인식 못하는 버그 수정(strict 모드 제거)

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './router.tsx'
@@ -6,10 +5,8 @@ import GlobalStyles from '@styles/GlobalStyles.ts'
 import { RecoilRoot } from 'recoil'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <RecoilRoot>
-      <GlobalStyles />
-      <RouterProvider router={router} />
-    </RecoilRoot>
-  </React.StrictMode>
+  <RecoilRoot>
+    <GlobalStyles />
+    <RouterProvider router={router} />
+  </RecoilRoot>
 )
